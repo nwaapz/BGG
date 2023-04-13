956362e49a8e93019541d9c2ba0bbb29bc376f4b
hosein abdl <53816663+nwaapz@users.noreply.github.com>
2023-04-14 03:39:18 +0430

    implementing-socket-io

--- server.js
+++ server.js
@@ -31,7 +31,30 @@ dbconnection.getConnection((err,connection)=> {
    connection.release();
 });
 
-const https=require('https');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + '/index.html');
+ });
+ 
+ io.on('connection', (socket) => {
+   console.log('a user connected');
+ });
+ 
+ server.listen(3000, () => {
+   console.log('listening on *:3000');
+ });
+
+
+
+
+
+
 const axios = require('axios');
 const FormData=require('form-data');
 let boards=new Array();
